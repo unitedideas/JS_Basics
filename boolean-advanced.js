ba2b224b7a77250eb8db9643385cfebf74c4452a
Unitedideas <unitedideas@gmail.com>
2019-03-31 14:27:23 -0700

    conditional if else-if else

---
+++ boolean-advanced.js
@@ -0,0 +1,10 @@
+let isAccountLocked = false
+let userRole = 'admin'
+
+if (isAccountLocked) {
+    console.log('The account is locked')
+} else if (userRole === 'admin') {
+    console.log('Welcome Admin')
+} else {
+    console.log('Welcome')
+}
