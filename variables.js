92d43a1f4732c1cad2b78e858c93c40cd467f917
Unitedideas <unitedideas@gmail.com>
2019-03-31 13:46:11 -0700

    variables

---
+++ variables.js
@@ -0,0 +1,10 @@
+// 1. You can't define a variable more than once
+
+let petName = 'Hal'
+petName = 'John'
+
+console.log(petName)
+
+// 2. There are rules related to variable names
+let a1$_ = 2
+let result = 3 + 4
