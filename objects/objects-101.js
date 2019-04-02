bb0a2f3309d193bbf68649b3fe04898e7a9e7f4d
Unitedideas <unitedideas@gmail.com>
2019-04-02 12:45:40 -0700

    objects-101

---
+++ objects/objects-101.js
@@ -0,0 +1,23 @@
+let myBook = {
+    title: '1984',
+    author: 'Gerore Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+//challenge
+
+let person = {
+    name: 'Shane',
+    age: 28,
+    location: 'Portland'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+person.age += 1
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
