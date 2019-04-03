2775e6bff8be709725c6bd0538264d0e938bfb36
Unitedideas <unitedideas@gmail.com>
2019-04-03 12:48:42 -0700

    arrays-methods

---
+++ arrays/notes.js
@@ -0,0 +1,15 @@
+const notes = ['note 1', 'note 2', 'note 3']
+
+// console.log(notes.pop())
+// notes.push('pushed note')
+
+// console.log(notes.shift())
+// notes.unshift('My first note')
+
+// notes.splice(1,1, 'marry')
+
+notes[0] = 'new note 1'
+
+console.log(notes.length)
+console.log(notes)
+
---
+++ arrays/todos.js
@@ -0,0 +1,12 @@
+// create array with 5 todos
+// print message - you have x todos
+// print the first and second to last item in the array
+
+const todo = ['walk dog', 'workout', 'study', 'send invites', 'attend meetup']
+
+console.log(`You have ${todo.length} in your todo list`)
+console.log(todo.splice(2, 1))
+console.log(todo.push('new push item'))
+console.log(todo.shift())
+
+console.log(todo)
